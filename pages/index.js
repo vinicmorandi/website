a61ec1c95c9d20332247028e1144143c2015c23e
Vinícius Morandi <viniciuscmorandi@gmail.com>
2022-11-20 20:20:04 -0300

    responsividade cagada

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Home from "./home"
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import { orange } from '@mui/material/colors';
+import { useState } from "react";
+import { useEffect } from "react";
 
 const theme = createTheme({
     palette: {
@@ -10,10 +12,22 @@ const theme = createTheme({
     },
 });
 
+
+
 export default function Index() {
+    const [width, setWidth] = useState(901)
+
+    useEffect(() => {
+        window.addEventListener('resize', setWidth(window.innerWidth));
+    }, [width])
+
     return (
         <ThemeProvider theme={theme}>
-            <Home />
+            {
+                width >= 1920
+                    ? <Home />
+                    : <center>too lazy to do proper responsivity lol</center>
+            }
         </ThemeProvider>
     )
 }
